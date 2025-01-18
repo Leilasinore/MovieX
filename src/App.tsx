169c579760030fe30ffb2681d4a1ad70703e5257
Leila Sinore <leilasinore99@gmail.com>
2025-01-18 12:54:15 +0300

    home page setup and API integration

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Route,Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/movie/:movieId" element={<Singlemovie />} />
+        <Route path="/movie/search/:movieName" element={<Results />} />
+        <Route path="*" element={<Displayerror />} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import Footersection from "../components/Footersection";
+import Headersection from "../components/Headersection";
+import Featured from "../components/Featured";
+import { useEffect, useState } from "react";
+import { moviesUrl } from "../Urlendpoint/Urlendpoint";
+
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [errors, setErrors] = useState([]);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(moviesUrl);
+      const data = await res.json();
+      const firstTen = data.results.slice(0, 10);
+      setMovies(firstTen);
+    } catch (error:any) {
+      setErrors(error.stack);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <>
+      <Headersection />
+      <Featured movies={movies} isLoading={isLoading} errors={errors} />
+      <Footersection />
+    </>
+  );
+};
+
+export default Home;
