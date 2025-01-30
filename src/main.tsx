c320ee747f87272caace051d44bbb7a1f59d4ed6
Leila Sinore <leilasinore99@gmail.com>
2025-01-30 20:51:26 +0300

    fixed build errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { Provider } from 'react-redux';
-import {store} from "./store/index.tsx"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./store/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <BrowserRouter>
-        <ToastContainer/>
+        <ToastContainer />
         <App />
       </BrowserRouter>
     </Provider>
